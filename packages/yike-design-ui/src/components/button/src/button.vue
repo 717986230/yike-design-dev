5f96a55692bf401fa9e92f269a0c733353c1179a
杨兴隆 <81056572+717986230@users.noreply.github.com>
2023-07-27 15:44:15 +0800

    feta:优化button.vue代码

--- packages/yike-design-ui/src/components/button/src/button.vue
+++ packages/yike-design-ui/src/components/button/src/button.vue
@@ -1,10 +1,15 @@
 <template>
   <button
     :class="[
-      bem([type, status, shape, size], {
-        loading: loading,
-        long: long,
-        disabled: disabled,
+      bem([
+        type,
+        status,
+        shape,
+        size,
+      ], {
+        loading,
+        long,
+        disabled,
       }),
     ]"
     :disabled="disabled || loading"
@@ -12,32 +17,28 @@
     <svg v-if="loading" viewBox="25 25 50 50">
       <circle r="20" cy="50" cx="50"></circle>
     </svg>
-    <slot name="icon"></slot>
-    <slot></slot>
+    <slot name="icon" />
+    <slot />
   </button>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'YKButton',
-}
-</script>
 <script setup lang="ts">
-import { ButtonProps } from './button'
-import { createCssScope } from '../../../utils/bem'
-import '../style'
+import { ButtonProps } from './button';
+import { createCssScope } from '../../../utils/bem';
+import '../style';
 
-const bem = createCssScope('button')
+const bem = createCssScope('button');
 
-defineOptions({
-  name: 'YKButton',
-})
-withDefaults(defineProps<ButtonProps>(), {
+const defaults: ButtonProps = {
   type: 'primary',
   size: 'l',
   shape: 'default',
   long: false,
   loading: false,
   disabled: false,
-})
+};
+
+const props = defineProps<ButtonProps>();
+
+const mergedProps = { ...defaults, ...props };
 </script>
